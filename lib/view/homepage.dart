5d73810e234460b53032085db435631f7f81c32d
sharmaprashant18 <ps3348644@gmail.com>
2024-06-20 19:16:17 +0545

    Changed

--- lib/view/homepage.dart
+++ lib/view/homepage.dart
@@ -1,35 +1,3 @@
-// import 'package:flutter/material.dart';
-
-// class HomePage extends StatelessWidget {
-//   const HomePage({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return SafeArea(
-//       child: Scaffold(
-//           appBar: AppBar(
-//             actions: [
-//               AnimatedContainer(
-//                 duration: const Duration(milliseconds: 5),
-//                 width: 50,
-//                 height: 50,
-//                 decoration: BoxDecoration(
-//                     color: Colors.blue,
-//                     borderRadius: BorderRadius.horizontal(
-//                         left: Radius.elliptical(10, 20))),
-//                 child: IconButton(
-//                   icon: const Icon(Icons.search),
-//                   color: Colors.white,
-//                   onPressed: () {},
-//                 ),
-//               ),
-//             ],
-//           ),
-//           body: Container()),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:movieapp/provider/movie_provider.dart';
